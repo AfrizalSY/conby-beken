6c4f593e97c7eb54c97acb53938317b9d267460b
aryyawijaya <aryyawijaya@gmail.com>
2021-07-29 12:36:11 +0700

    create public controller for find 5 top consultants

---
+++ app/controller/public.js
@@ -0,0 +1,20 @@
+const Consultant = require('../models/consultant');
+
+exports.findTop5Consultants = (req, res) => {
+    // sort by rating
+    Consultant.find({})
+        .sort({ rating: 1 })
+        .select({ rating: 1, name: 1, subSpecialist: 1, price: 1 })
+        .limit(5)
+        .then((consultants) => {
+            return res.status(200).json({
+                message: 'success!',
+                data: consultants
+            });
+        }).catch((err) => {
+            console.log('>> error: ', err);
+            return res.status(500).json({
+                error: err.message
+            });
+        });
+};
