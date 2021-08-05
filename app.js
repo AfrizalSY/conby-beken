492ec486da47903c5e9cefe182fb54f1ac551df3
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2021-08-05 15:46:17 +0800

    port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
