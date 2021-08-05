722649b6c784095c16e0f3d4d19d0306cc2bee05
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2021-08-06 00:12:15 +0800

    Revert "Merge branch 'master' of https://github.com/SE-SundaEmpire/conby-beken"
    This reverts commit 8019118105fc87c32dce7b898a57e4b3cbabeee3, reversing
    changes made to bed4e6f5dde7d38ff11e0808e86ccf2b7b9c81ca.

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const controller = require('../controller/user');
+const upload = require('../middlewares/photo');
 const uploadPhoto = require('../middlewares/photo');
 
 //Login Page
