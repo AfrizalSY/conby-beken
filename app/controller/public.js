78dfecd15d492cae1f56724c0a48868123407103
aryyawijaya <aryyawijaya@gmail.com>
2021-08-11 09:43:22 +0700

    edit public controller to dont show id

--- app/controller/public.js
+++ app/controller/public.js
@@ -4,7 +4,7 @@ exports.findTop4Consultants = (req, res) => {
     // sort by rating
     Consultant.find({})
         .sort({ rating: 1 })
-        .select({ rating: 1, photo: 1, name: 1, subSpecialist: 1, price: 1 })
+        .select({ rating: 1, photo: 1, name: 1, subSpecialist: 1, price: 1, _id: 0 })
         .limit(4)
         .then((consultants) => {
             res.status(200).json({
@@ -17,7 +17,7 @@ exports.findTop4Consultants = (req, res) => {
 
 exports.findAllConsultants = (req, res) => {
     Consultant.find({})
-        .select({ rating: 1, photo: 1, name: 1, subSpecialist: 1, price: 1 })
+        .select({ rating: 1, photo: 1, name: 1, subSpecialist: 1, price: 1, _id: 0 })
         .then((consultants) => {
             res.status(200).json({
                 status: 200,
