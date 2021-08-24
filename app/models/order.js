5d007179ddab2900b0195cfd419f3d83ee1d9eee
aryyawijaya <aryyawijaya@gmail.com>
2021-08-24 17:14:53 +0700

    edit endpoint get order history user and edit model user

--- app/models/order.js
+++ app/models/order.js
@@ -20,9 +20,6 @@ const OrderSchema = new mongoose.Schema(
             type: String,
             required: true
         },
-        evidenceOfTransfer: {
-            type: String
-        },
         price: {
             type: Number
         },
